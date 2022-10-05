5bd12b218a1d4899460f800abc71687831afb85a
CarlosSallan <113118603+CarlosSallan@users.noreply.github.com>
2022-10-05 20:08:21 +0200

    locoo

--- src/cript_main/CriptMain.java
+++ src/cript_main/CriptMain.java
@@ -1,10 +1,11 @@
 package cript_main;
 
+import cript_object.CriptObject;
+
 public class CriptMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
--- src/cript_object/CriptObject.java
+++ src/cript_object/CriptObject.java
@@ -3,9 +3,43 @@ package cript_object;
 public class CriptObject {
 	private String mnsj_original;
 	private String mnsj_final;
-	private char[] alfabeto = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z', 'a', 'b',
+	private static char[] alfabeto = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z', 'a', 'b',
 			'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
 			'8', '9', '?', '!', '.', ',', '_'};
 	
+	//Inicializador
+	public CriptObject(String mnsj) {
+		this.mnsj_original = mnsj;
+		this.mnsj_final = mnsj;
+	}
+	
+	//4 getters
+	public static String getAlfabeto() {
+		String alfaString = "";
+		
+		for(int x = 0;x < alfabeto.length; x++) {
+			alfaString = alfaString.concat(String.valueOf(alfabeto[x]));
+		}
+		return alfaString;
+	}
+	public String getMensajeOriginal() {
+		return mnsj_original;
+	}
+	
+	public String getMensajeFinal() {
+		return mnsj_final;
+	}
+	
+	/*
+	 -Getter patron transformaciones
+	 * */
+	
+	//Algoritmo Cesar
+	public void algCesar(int clave) {
+	}
+	
 	//Pruebas
+	public static void main(String[] args) {
+		System.out.println(getAlfabeto());
+	}
 }
