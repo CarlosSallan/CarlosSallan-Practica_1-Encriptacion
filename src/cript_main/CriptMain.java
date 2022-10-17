22c09627062fa1ffaa986bf654dea9bdbd6f4e1c
CarlosSallan <113118603+CarlosSallan@users.noreply.github.com>
2022-10-17 11:17:58 +0200

    cesar oololollolo

--- src/cript_main/CriptMain.java
+++ src/cript_main/CriptMain.java
@@ -5,9 +5,10 @@ import cript_object.CriptObject;
 public class CriptMain {
 
 	public static void main(String[] args) {
-		CriptObject prueba = new CriptObject();
-		
-		System.out.println(prueba.getAlfabeto());
+		CriptObject prueba = new CriptObject("hola");
+		prueba.algCesar(3);
+		System.out.println(prueba.getMensajeFinal());
+		prueba.getPatron();
 	}
 
 }
--- src/cript_object/CriptObject.java
+++ src/cript_object/CriptObject.java
@@ -7,7 +7,8 @@ import java.util.Map;
 public class CriptObject {
 	private String mnsj_original;
 	private String mnsj_final;
-	private Map<String, String> Patron = new LinkedHashMap<String, String>();
+	//<"Nombre", "clave">
+	private Map<String, Object> Patron = new LinkedHashMap<String, Object>();
 	private static char[] alfabeto = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z', 'a', 'b',
 			'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
 			'8', '9', '?', '!', '.', ',', '_'};
@@ -41,12 +42,30 @@ public class CriptObject {
 	
 	public void getPatron() {
 		for(String x : Patron.keySet()) {
-			System.out.println(Patron.get(x));
+			System.out.println("Nombre:" + x + " Clave:" +Patron.get(x));
 		}
 	}
 	
 	//Algoritmo Cesar
 	public void algCesar(int clave) {
+		String mnsj_encrip = "";
+		
+		for(int x=0;x < mnsj_original.length();x++) {
+			char letra = mnsj_original.charAt(x);
+			
+			for(int y=0;y < alfabeto.length;y++) {
+				
+				if(letra == alfabeto[y]) {
+					
+					letra = alfabeto[y + clave];
+					mnsj_encrip =  mnsj_encrip.concat(Character.toString(letra));
+					y = alfabeto.length;
+				}
+			}
+		}
+		
+		mnsj_final = mnsj_encrip;
+		Patron.put("Cesar", clave);
 	}
 	
 	public void desCesar() {
@@ -74,9 +93,5 @@ public class CriptObject {
 	//Recuperacion
 	public void recTransformacion() {
 		
-	}
-	//Pruebas
-	private static void main(String[] args) {
-		System.out.println(getAlfabeto());
 	}
 }
